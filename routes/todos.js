4d16a8b4ccb9caac92b111803aa6975b50713db9
dev637 <devang713@gmail.com>
2018-11-12 13:47:45 -0500

    Define show route

--- routes/todos.js
+++ routes/todos.js
@@ -25,4 +25,13 @@ router.post('/', (req, res) => {
   console.log(req.body);
 });
 
+router.get('/:todoId', (req, res) => {
+  db.Todo.findById(req.params.todoId)
+    .then(foundTodo => {
+      res.json(foundTodo);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
 module.exports = router;
