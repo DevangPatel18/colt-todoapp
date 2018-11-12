8e43a4dbe62b01a0eb6e7510f36376157c5bf8f9
dev637 <devang713@gmail.com>
2018-11-12 18:34:48 -0500

    Add createTodo methods

--- public/main.js
+++ public/main.js
@@ -1,13 +1,33 @@
 $(document).ready(() => {
   $.getJSON('/api/todos').then(addTodos);
+
+  $('#todoInput').keypress(event => {
+    if (event.which === 13) {
+      createTodo();
+    }
+  });
 });
 
 const addTodos = todos => {
-  todos.forEach(todo => {
-    const newTodo = $(`<li class="task">${todo.name}</li>`);
-    if(todo.completed){
-      newTodo.addClass('done')
-    }
-    $('.list').append(newTodo);
-  });
+  todos.forEach(addTodo);
+};
+
+const addTodo = todo => {
+  const newTodo = $(`<li class="task">${todo.name}</li>`);
+  if (todo.completed) {
+    newTodo.addClass('done');
+  }
+  $('.list').append(newTodo);
+};
+
+const createTodo = () => {
+  const usrInput = $('#todoInput').val();
+  $.post('/api/todos', { name: usrInput })
+    .then(newTodo => {
+      $('#todoInput').val('');
+      addTodo(newTodo);
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
